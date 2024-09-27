c50316a7432169301250090c3ab09af25b6c2ae2
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2024-09-27 08:06:49 +0200

    Add fish config

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    abbr -a gst git status
+    abbr -a g git
+    abbr -a gst git status
+    abbr -a ga git add
+    abbr -a gdf git diff
+    abbr -a gfe git fetch
+    abbr -a gpl git pull
+    abbr -a gp git push
+    abbr -a gco git checkout
+    abbr -a gc git commit
+    abbr -a gcm git commit -m
+    abbr -a gbr git branch
+    abbr -a glg git log
+    abbr -a gmr git merge
+    abbr -a grb git rebase
+    abbr -a gsw git switch
+    abbr -a gcl git clone
+    abbr -a grst git reset
+end
---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+set fish_greeting
+
+# Activate mise
+mise activate fish | source
+
+# Init starship
+starship init fish | source
