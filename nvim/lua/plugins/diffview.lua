1b68aabcace84abe3d0c82dd478932f8975f1aa1
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-03-27 15:43:25 +0100

    nvim: add diffview

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_create_user_command("DiffviewToggle", function(e)
+	local view = require("diffview.lib").get_current_view()
+
+	if view then
+		vim.cmd("DiffviewClose")
+	else
+		vim.cmd("DiffviewOpen " .. e.args)
+	end
+end, { nargs = "*" })
+
+return {
+	"sindrets/diffview.nvim",
+	version = false,
+	lazy = true,
+	keys = {
+		{ "<Leader>hd", "<cmd>DiffviewToggle<cr>", { desc = "Show git diff" } },
+	},
+	opts = {
+		enhanced_diff_hl = true,
+	},
+}
