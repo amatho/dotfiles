be5b63522f6a536b57265e084fe6842fbae6a340
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-04-05 13:48:10 +0200

    nvim: set tabsize to 4

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,9 +9,9 @@ opt.autoindent = true
 opt.smartindent = true
 opt.expandtab = true
 opt.shiftround = true
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.softtabstop = 2
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
