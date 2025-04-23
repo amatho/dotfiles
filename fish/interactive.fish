bfffef265bd10cba69f052cf30ee16ff5e54bb20
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-04-23 11:47:09 +0200

    Add function to auto rename Zellij tab

--- fish/interactive.fish
+++ fish/interactive.fish
@@ -53,3 +53,35 @@ abbr -a tfa terraform apply
 abbr -a tfi terraform init
 abbr -a tfp terraform plan
 abbr -a tf terraform
+
+# Automatic renaming of Zellij tab to Git repo name, or current folder if no Git repo exists
+if set -q ZELLIJ
+    function __zellij_tab_name_update --on-variable PWD
+        set current_dir $PWD
+        if test $current_dir = $HOME
+            set tab_name "~"
+        else
+            set tab_name (basename $current_dir)
+        end
+
+        if fish_git_prompt >/dev/null
+            # we are in a git repo
+
+            # if we are in a git superproject, use the superproject name
+            # otherwise, use the toplevel repo name
+            set git_root (git rev-parse --show-superproject-working-tree)
+            if test -z $git_root
+                set git_root (git rev-parse --show-toplevel)
+            end
+
+            #  if we are in a subdirectory of the git root, use the relative path
+            if test (string lower "$git_root") != (string lower "$current_dir")
+                set tab_name (basename $git_root)/(basename $current_dir)
+            end
+        end
+
+        nohup zellij action rename-tab $tab_name >/dev/null 2>&1
+    end
+
+    __zellij_tab_name_update
+end
