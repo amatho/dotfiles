be73db8d60e114ac9f1e1a75f9477a66cd35395f
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-05-08 11:33:06 +0200

    nvim: use Snacks instead of fzf for neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -2,11 +2,14 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"ibhagwan/fzf-lua",
 	},
 	lazy = true,
 	keys = {
 		{ "<leader>hg", "<cmd>Neogit<cr>", { desc = "Neogit" } },
 	},
-	opts = {},
+	opts = {
+		integrations = {
+			snacks = true,
+		},
+	},
 }
