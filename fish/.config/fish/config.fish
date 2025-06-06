824ff6055d74c47ed43b5b543afe9ec8c6188d0a
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-06-06 12:37:34 +0200

    fish: improve startup time for non-login and non-interactive shells

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,22 +1,26 @@
-set fish_greeting
+if status is-login
+    switch (uname)
+        case Darwin
+            source $__fish_config_dir/darwin.fish
+    end
 
-switch (uname)
-    case Darwin
-        source $__fish_config_dir/darwin.fish
+    # Make global Mise tools available in system PATH
+    mise hook-env -s fish | source
+
+    source ~/.cargo/env.fish
+
+    fish_add_path -P ~/.local/share/bob/nvim-bin
+    set -x EDITOR "nvim"
 end
 
-# Activate mise
-mise activate fish | source
-mise hook-env -s fish | source
-
-source ~/.cargo/env.fish
-
-fish_add_path -P ~/.local/share/bob/nvim-bin
-set -x EDITOR "nvim"
-
-zoxide init fish --cmd cd | source
-starship init fish | source
-
 if status is-interactive
+    set fish_greeting
+
+    # Activate Mise for interactive shells
+    mise activate fish | source
+
+    zoxide init fish --cmd cd | source
+    starship init fish | source
+
     source $__fish_config_dir/interactive.fish
 end
