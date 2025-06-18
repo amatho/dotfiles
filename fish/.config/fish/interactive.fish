76359c8c6ad420ade45f2ef22afa5561594fd047
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-06-18 13:44:42 +0200

    fish: check universal variables into Git

--- fish/.config/fish/interactive.fish
+++ fish/.config/fish/interactive.fish
@@ -1,32 +1,3 @@
-set -g fish_color_autosuggestion brblack
-set -g fish_color_cancel -r
-set -g fish_color_command brgreen
-set -g fish_color_comment brmagenta
-set -g fish_color_cwd green
-set -g fish_color_cwd_root red
-set -g fish_color_end brmagenta
-set -g fish_color_error brred
-set -g fish_color_escape brcyan
-set -g fish_color_history_current --bold
-set -g fish_color_host normal
-set -g fish_color_host_remote yellow
-set -g fish_color_match --background=brblue
-set -g fish_color_normal normal
-set -g fish_color_operator cyan
-set -g fish_color_param brwhite
-set -g fish_color_quote yellow
-set -g fish_color_redirection bryellow
-set -g fish_color_search_match white --background=brblack
-set -g fish_color_selection white --bold --background=brblack
-set -g fish_color_status red
-set -g fish_color_user brgreen
-set -g fish_color_valid_path --underline
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description yellow
-set -g fish_pager_color_prefix white --bold --underline
-set -g fish_pager_color_progress brwhite --background=cyan
-set -g fish_pager_color_selected_background -r
-
 alias ls="eza"
 alias la="eza -la"
 alias ll="eza -l"
