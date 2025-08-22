507de67c44dcd5bd965aea92650bde68625e3bf1
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-08-22 14:26:43 +0200

    nvim: set codecompanion model to claude

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,11 +1,20 @@
 return {
 	"olimorris/codecompanion.nvim",
-	keys = {
-		{ "<leader>c", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Code Companion" },
-	},
-	opts = {},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
+	},
+	keys = {
+		{ "<leader>c", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Code Companion" },
+	},
+	opts = {
+		strategies = {
+			chat = {
+				adapter = {
+					name = "copilot",
+					model = "claude-sonnet-4",
+				},
+			},
+		},
 	},
 }
