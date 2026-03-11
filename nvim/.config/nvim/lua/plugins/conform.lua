423e1e9df548188295317f1200061b6f3b0f1848
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2026-03-11 12:09:24 +0100

    nvim: fix conform oxfmt settings and stop after first formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -14,59 +14,52 @@ return {
 			},
 		},
 		opts = {
-			notify_on_error = true,
 			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
 				local disable_filetypes = { c = true, cpp = true }
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
 				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
+					return { timeout_ms = 1000, lsp_format = "fallback" }
 				end
 			end,
+			notify_on_error = true,
+			stop_after_first = true,
 			formatters = {
+				oxfmt = {
+					require_cwd = true,
+				},
 				prettier = {
 					require_cwd = true,
 				},
 				prettierd = {
 					require_cwd = true,
 				},
+				biome = {
+					require_cwd = true,
+				},
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true, stop_after_first = true },
-				--false
 				sh = { "shfmt" },
-				["javascript"] = { "prettierd", "prettier", stop_after_first = true },
-				["javascriptreact"] = { "prettierd", "prettier", stop_after_first = true },
-				["typescript"] = { "prettierd", "prettier", stop_after_first = true },
-				["typescriptreact"] = { "prettierd", "prettier", stop_after_first = true },
-				["vue"] = { "prettierd", "prettier", stop_after_first = true },
-				["css"] = { "prettierd", "prettier", stop_after_first = true },
-				["scss"] = { "prettierd", "prettier", stop_after_first = true },
-				["less"] = { "prettierd", "prettier", stop_after_first = true },
-				["html"] = { "prettierd", "prettier", stop_after_first = true },
-				["json"] = { "prettierd", "prettier", stop_after_first = true },
-				["jsonc"] = { "prettierd", "prettier", stop_after_first = true },
-				["yaml"] = { "prettierd", "prettier", stop_after_first = true },
-				["markdown"] = { "prettierd", "prettier", stop_after_first = true },
-				["markdown.mdx"] = { "prettierd", "prettier", stop_after_first = true },
-				["graphql"] = { "prettierd", "prettier", stop_after_first = true },
-				["handlebars"] = { "prettierd", "prettier", stop_after_first = true },
-				["python"] = { "ruff-lsp" },
+				["javascript"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["javascriptreact"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["typescript"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["typescriptreact"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["vue"] = { "prettierd", "prettier" },
+				["css"] = { "oxfmt", "prettierd", "prettier" },
+				["scss"] = { "oxfmt", "prettierd", "prettier" },
+				["less"] = { "oxfmt", "prettierd", "prettier" },
+				["html"] = { "oxfmt", "prettierd", "prettier" },
+				["json"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["jsonc"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["yaml"] = { "oxfmt", "prettierd", "prettier" },
+				["markdown"] = { "oxfmt", "prettierd", "prettier" },
+				["markdown.mdx"] = { "oxfmt", "prettierd", "prettier" },
+				["graphql"] = { "oxfmt", "biome", "prettierd", "prettier" },
+				["handlebars"] = { "prettierd", "prettier" },
 				["rust"] = { "rustfmt" },
 				["go"] = { "goimports", "gofumpt" },
 				["toml"] = { "taplo" },
-				["nix"] = { "nixfmt" },
 			},
 		},
 	},
