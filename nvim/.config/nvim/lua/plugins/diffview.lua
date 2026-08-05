27e42a74e3ffb852d19038fb85b69e4316c5ae12
Amandus Søve Thorsrud <amandus@thorsrud.dev>
2026-08-05 09:06:24 +0200

    jj/nvim: use diffview as merge tool

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -10,5 +10,12 @@ return {
 		"DiffviewFileHistory",
 		"DiffviewDiffFiles",
 		"DiffviewLog",
+		"DiffviewMergeFiles",
+	},
+	opts = {
+		enhanced_diff_hl = true,
+		view = {
+			merge_tool = { layout = "diff4_mixed" },
+		},
 	},
 }
